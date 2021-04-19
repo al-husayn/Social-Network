5b30e0c5893d263ada75db1207dbbb6e3f090053
al-husayn <al-hussein.abubakar@amalitech.org>
2021-04-19 14:08:03 +0000

    grap input user - search

--- js/script.js
+++ js/script.js
@@ -1,23 +1,82 @@
+var users = [
+    {
+        name: 'Sawda',
+        gender: 'F',
+        hobby: 'pets',
+        avatar: 'avatar1.png'        
+    },
+    {
+        name: 'Aphya',
+        gender: 'F',
+        hobby: 'pets',
+        avatar: 'avatar2.png'
+    },
+    {
+        name: 'Drake',
+        gender: 'M',
+        hobby: 'music',
+        avatar: 'avatar5.png'
+    },
+    {
+        name: 'Ibra',
+        gender: 'M',
+        hobby: 'sports',
+        avatar: 'avatar6.png'
+    },
+    {
+        name: 'Mercy',
+        gender: 'F',
+        hobby: 'reading',
+        avatar: 'avatar3.png'
+    },
+    {
+        name: 'Ama Konadu',
+        gender: 'F',
+        hobby: 'shopping',
+        avatar: 'avatar4.png'
+    },
+    {
+        name: 'Ridwan',
+        gender: 'M',
+        hobby: 'pets',
+        avatar: 'avatar7.png'
+    },
+];
+
 window.addEventListener('load', function() {
     
-    let reults = document.getElementById('results');
+    var results = document.getElementById('results');
+    
     function search() {
-
-         //get hobby
+        
+        //get hobby
         var hobbyField = document.getElementById('hobby');
         var hobby = hobbyField.value;
-        console.log(hobby);
+        
+        //get gender
+        var genderField = document.getElementById('gender');
+        var s = genderField.selectedIndex;
+        var gender = genderField.options[s].value;
+        
+        var resultsHtml = '';
+        var usersLength = users.length;
 
-        // get gender
-        let genderField = document.getElementById('gender');
-        let s  = genderField.selectedIndex;
-        let gender = genderField.options[s].value;
-        console.log(gender);
-
-        console.log('Im searching!');
-        reults.innerHTML ='<div style=" color: red;">hello world </div> <br/> new line<div class="person-row">some stuffs</div>';
-        console.log(reults);
-
+        for(var i = 0; i < usersLength; i++) {
+            //check gender
+            if (gender == 'A' || gender == users[i].gender) {
+                //check hobby
+                if (hobby == '' || hobby == users[i].hobby) {
+                    resultsHtml += '<div class="person-row">\
+                           <img src="images/' + users[i].avatar + '" />\
+                           <div class="person-info">\
+                           <div>' + users[i].name + '</div>\
+                           <div>' + users[i].hobby + '</div></div>\
+                            <button>Add as friend</button></div>';  
+                }
+            }
+        }
+        
+        results.innerHTML = resultsHtml;
     }
     
     var searchBtn = document.getElementById('searchBtn');
