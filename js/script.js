b0edc39cd443faeb9b8d1cb8437a11131da9ed71
al-husayn <al-hussein.abubakar@amalitech.org>
2021-04-19 06:32:42 +0000

    modifying DOM

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', function() {
     
+    let reults = document.getElementById('results');
     function search() {
         console.log('Im searching!');
+        reults.innerHTML ='<div style=" color: red;">hello world </div> <br/> new line<div class="person-row">some stuffs</div>';
+        console.log(reults);
+
     }
     
     var searchBtn = document.getElementById('searchBtn');
