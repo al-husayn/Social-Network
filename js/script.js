94deb7a488899552bb5223c386d45f784cd9b89c
al-husayn <al-hussein.abubakar@amalitech.org>
2021-04-19 07:11:36 +0000

    user input

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ window.addEventListener('load', function() {
     
     let reults = document.getElementById('results');
     function search() {
+
+         //get hobby
+        var hobbyField = document.getElementById('hobby');
+        var hobby = hobbyField.value;
+        console.log(hobby);
+
+        // get gender
+        let genderField = document.getElementById('gender');
+        let s  = genderField.selectedIndex;
+        let gender = genderField.options[s].value;
+        console.log(gender);
+
         console.log('Im searching!');
         reults.innerHTML ='<div style=" color: red;">hello world </div> <br/> new line<div class="person-row">some stuffs</div>';
         console.log(reults);
